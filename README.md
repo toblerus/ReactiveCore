bd2cbdbbab396da601f6d132b86352d511da4fbd
toblerus <37512320+toblerus@users.noreply.github.com>
2025-05-31 21:21:22 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,63 @@
+# ReactiveCore
+
+**ReactiveCore** is a lightweight reactive state system for Unity. It offers a simple `ReactiveValue<T>` class that allows for clean, safe value subscriptions and automatic lifecycle handling.
+
+## Features
+
+- Type-safe reactive values
+- Auto-disposal of subscriptions
+- Lightweight and dependency-free
+- Great for decoupling UI and logic
+
+## Installation
+
+1. Add this package via Unity Package Manager by Git URL: https://github.com/toblerus/reactivecore.git
+
+Or embed it in your `Packages/` folder.
+
+## Usage
+### Subscribe
+```
+public class Example : MonoBehaviour
+{
+    private ReactiveValue<int> health = new ReactiveValue<int>(100);
+    private IDisposable subscription;
+
+    void Start()
+    {
+        subscription = health.Subscribe(OnHealthChanged);
+        health.Value = 75;
+    }
+
+    void OnDestroy()
+    {
+        subscription.Dispose();
+    }
+
+    void OnHealthChanged(int newHealth)
+    {
+        Debug.Log("Health is now: " + newHealth);
+    }
+}
+```
+
+#### Console Output:
+
+```
+Health is now: 100
+
+Health is now: 75
+```
+### SkipValueOnSubscribe
+
+```
+void Start()
+{
+    subscription = health.SkipValueOnSubscribe(OnHealthChanged);
+    health.Value = 50;
+}
+```
+
+#### Console Output:
+
+`Health is now: 50`
---
+++ Runtime/ReactiveValue.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReactiveValue<T>
+{
+    private T _value;
+    private List<Subscription> _subscriptions = new List<Subscription>();
+
+    public ReactiveValue(T initialValue = default)
+    {
+        _value = initialValue;
+    }
+
+    private IDisposable Subscribe(Action<T> onValueChanged, bool skipCurrentValue)
+    {
+        var sub = new Subscription(onValueChanged, this);
+        _subscriptions.Add(sub);
+        if (!skipCurrentValue)
+            onValueChanged(_value);
+        return sub;
+    }
+    
+    public IDisposable Subscribe(Action<T> onValueChanged)
+    {
+        return Subscribe(onValueChanged, false);
+    }
+
+    public IDisposable SkipValueOnSubscribe(Action<T> onValueChanged)
+    {
+        return Subscribe(onValueChanged, true);
+    }
+
+    public T Value
+    {
+        get => _value;
+        set
+        {
+            if (!EqualityComparer<T>.Default.Equals(_value, value))
+            {
+                _value = value;
+                NotifySubscribers();
+            }
+        }
+    }
+
+    private void NotifySubscribers()
+    {
+        foreach (var sub in _subscriptions.ToArray())
+        {
+            sub.Callback?.Invoke(_value);
+        }
+    }
+
+    private void Unsubscribe(Subscription sub)
+    {
+        _subscriptions.Remove(sub);
+    }
+
+    private class Subscription : IDisposable
+    {
+        public Action<T> Callback { get; private set; }
+        private ReactiveValue<T> _parent;
+
+        public Subscription(Action<T> callback, ReactiveValue<T> parent)
+        {
+            Callback = callback;
+            _parent = parent;
+        }
+
+        public void Dispose()
+        {
+            _parent.Unsubscribe(this);
+            Callback = null;
+            _parent = null;
+        }
+    }
+}
