f1fbb928d2e303f999e8c1c464d60e05d5ee4f3b
toblerus <37512320+toblerus@users.noreply.github.com>
2025-06-01 03:26:11 +0200

    Moves RactiveValue to namespace

--- Runtime/ReactiveValue.cs
+++ Runtime/ReactiveValue.cs
@@ -2,77 +2,80 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ReactiveValue<T>
+namespace ReactiveCore
 {
-    private T _value;
-    private List<Subscription> _subscriptions = new List<Subscription>();
+    public class ReactiveValue<T>
+    {
+        private T _value;
+        private List<Subscription> _subscriptions = new List<Subscription>();
 
-    public ReactiveValue(T initialValue = default)
-    {
-        _value = initialValue;
-    }
-
-    private IDisposable Subscribe(Action<T> onValueChanged, bool skipCurrentValue)
-    {
-        var sub = new Subscription(onValueChanged, this);
-        _subscriptions.Add(sub);
-        if (!skipCurrentValue)
-            onValueChanged(_value);
-        return sub;
-    }
-    
-    public IDisposable Subscribe(Action<T> onValueChanged)
-    {
-        return Subscribe(onValueChanged, false);
-    }
-
-    public IDisposable SkipValueOnSubscribe(Action<T> onValueChanged)
-    {
-        return Subscribe(onValueChanged, true);
-    }
-
-    public T Value
-    {
-        get => _value;
-        set
+        public ReactiveValue(T initialValue = default)
         {
-            if (!EqualityComparer<T>.Default.Equals(_value, value))
+            _value = initialValue;
+        }
+
+        private IDisposable Subscribe(Action<T> onValueChanged, bool skipCurrentValue)
+        {
+            var sub = new Subscription(onValueChanged, this);
+            _subscriptions.Add(sub);
+            if (!skipCurrentValue)
+                onValueChanged(_value);
+            return sub;
+        }
+
+        public IDisposable Subscribe(Action<T> onValueChanged)
+        {
+            return Subscribe(onValueChanged, false);
+        }
+
+        public IDisposable SkipValueOnSubscribe(Action<T> onValueChanged)
+        {
+            return Subscribe(onValueChanged, true);
+        }
+
+        public T Value
+        {
+            get => _value;
+            set
             {
-                _value = value;
-                NotifySubscribers();
+                if (!EqualityComparer<T>.Default.Equals(_value, value))
+                {
+                    _value = value;
+                    NotifySubscribers();
+                }
             }
         }
-    }
 
-    private void NotifySubscribers()
-    {
-        foreach (var sub in _subscriptions.ToArray())
+        private void NotifySubscribers()
         {
-            sub.Callback?.Invoke(_value);
-        }
-    }
-
-    private void Unsubscribe(Subscription sub)
-    {
-        _subscriptions.Remove(sub);
-    }
-
-    private class Subscription : IDisposable
-    {
-        public Action<T> Callback { get; private set; }
-        private ReactiveValue<T> _parent;
-
-        public Subscription(Action<T> callback, ReactiveValue<T> parent)
-        {
-            Callback = callback;
-            _parent = parent;
+            foreach (var sub in _subscriptions.ToArray())
+            {
+                sub.Callback?.Invoke(_value);
+            }
         }
 
-        public void Dispose()
+        private void Unsubscribe(Subscription sub)
         {
-            _parent.Unsubscribe(this);
-            Callback = null;
-            _parent = null;
+            _subscriptions.Remove(sub);
+        }
+
+        private class Subscription : IDisposable
+        {
+            public Action<T> Callback { get; private set; }
+            private ReactiveValue<T> _parent;
+
+            public Subscription(Action<T> callback, ReactiveValue<T> parent)
+            {
+                Callback = callback;
+                _parent = parent;
+            }
+
+            public void Dispose()
+            {
+                _parent.Unsubscribe(this);
+                Callback = null;
+                _parent = null;
+            }
         }
     }
 }
